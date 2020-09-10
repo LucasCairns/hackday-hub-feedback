cf9da8a8fd451fdbe703f83a3e29619b8549cf00
Lucas Cairns <lucas.cairns@gmail.com>
2020-09-10 14:23:54 +0100

    Udpate Feedback entity to include required fields

--- src/database/entities/feedback.ts
+++ src/database/entities/feedback.ts
@@ -1,10 +1,34 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Feedback {
-  @PrimaryGeneratedColumn()
+  @PrimaryColumn('uuid')
   id!: number;
 
   @Column()
   title!: string;
+
+  @Column()
+  URL!: string;
+
+  @Column()
+  contentType!: string;
+
+  @Column()
+  sentiment!: string;
+
+  @Column('time with time zone')
+  date!: string;
+
+  @Column()
+  establishment!: string;
+
+  @Column('uuid')
+  sessionID!: string;
+
+  @Column('jsonb')
+  categories!: [string];
+
+  @Column('jsonb')
+  secondaryTags!: [string];
 }
